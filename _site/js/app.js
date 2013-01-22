c9d02cfd8ef46af7bfebac7af9476727e5e2a29d
sethvincent <sethvincent@gmail.com>
2013-01-22 13:23:01 -0800

    revise legislator details card

--- _site/js/app.js
+++ _site/js/app.js
@@ -1,12 +1,9 @@
 $(document).ready(function(){
   
-  var items = [];
   var template = _.template( $('#legislator-template').html() );
   
-  $(".legislators").on('mouseleave', function(){
-    $(this).children(".legislators-detail").hide();
-  });
-  
+  // get all the legislators and put them on the page
+  var items = [];
   $.getJSON('wa-legislators.json', function(data){
     var legislators = data['rows'];
     
@@ -18,5 +15,23 @@ $(document).ready(function(){
       'class': 'legislators',
       html: items.join('')
     }).appendTo('.container');
+    
+    // handle popup of infobox
+    $('.legislator').click(function(event){
+      event.stopPropagation();
+      console.log("pizza");
+      $('.legislator-details').not( $(this).children() ).hide();
+      $(this).children('.legislator-details').show();
+    })
+    
+    $('.close').click(function(event){
+      event.stopPropagation();
+      
+      $(this).parent().hide();
+    });
+    
+    $('html').click(function(){
+      $('.legislator-details').hide();
+    });
   });
 });
