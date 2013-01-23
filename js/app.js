f45a79580d74941c0495194c3fc9565de4b495ea
sethvincent <sethvincent@gmail.com>
2013-01-22 18:01:07 -0800

    switch to using tabletop.js with a google spreadsheet

--- js/app.js
+++ js/app.js
@@ -1,37 +1,41 @@
-$(document).ready(function(){
+(function($) {
   
-  var template = _.template( $('#legislator-template').html() );
-  
-  // get all the legislators and put them on the page
-  var items = [];
-  $.getJSON('wa-legislators.json', function(data){
-    var legislators = data['rows'];
-    
-    $.each(legislators, function(key, legislator){
-      items.push( template(legislator) );
-    });
-    
-    $('<div/>', {
-      'class': 'legislators',
-      html: items.join('')
-    }).appendTo('.container');
-    
-    // handle popup of infobox
-    $('.legislator').click(function(event){
-      event.stopPropagation();
-      console.log("pizza");
-      $('.legislator-details').not( $(this).children() ).hide();
-      $(this).children('.legislator-details').show();
-    })
-    
-    $('.close').click(function(event){
-      event.stopPropagation();
+  Tabletop.init({ 
+    key: '0AuwSoq4skLXjdHhjNFA1c3N4eW03dUVqSm5jTEN5alE',
+    callback: function(data, tabletop) {
       
-      $(this).parent().hide();
-    });
-    
-    $('html').click(function(){
-      $('.legislator-details').hide();
-    });
+      var items = [];
+      var template = _.template( $('#legislator-template').html() );
+      var legislators = data;
+      
+      $.each(legislators, function(key, legislator){
+        items.push( template(legislator) );
+      });
+      
+      $('<div/>', {
+        'class': 'legislators',
+        html: items.join('')
+      }).appendTo('.container');
+      
+      // handle popup of infobox
+      $('.legislator').click(function(event){
+        event.stopPropagation();
+        console.log("pizza");
+        $('.legislator-details').not( $(this).children() ).hide();
+        $(this).children('.legislator-details').show();
+      })
+      
+      $('.close').click(function(event){
+        event.stopPropagation();
+        
+        $(this).parent().hide();
+      });
+      
+      $('html').click(function(){
+        $('.legislator-details').hide();
+      });
+      
+    },
+    simpleSheet: true 
   });
-});
+})(jQuery);
